d977b8424c3cc7f7bb72772306027293b996fc76
Aria Moraine <ariamoraine@gmail.com>
2012-10-24 13:16:48 -0400

    Fixed some of the UI

--- gen/knitting/counter/R.java
+++ gen/knitting/counter/R.java
@@ -18,34 +18,33 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int knit_texture=0x7f020002;
+        public static final int ic_launcherold=0x7f020002;
+        public static final int knit_texture=0x7f020003;
     }
     public static final class id {
-        public static final int clearButton=0x7f070002;
-        public static final int minusRepeatButton=0x7f070005;
-        public static final int minusRowButton=0x7f070003;
+        public static final int clearButton=0x7f070000;
+        public static final int minusRepeatButton=0x7f070007;
+        public static final int minusRowButton=0x7f070005;
         public static final int repeatNumber=0x7f070008;
-        public static final int repeatPlusButton=0x7f070000;
-        public static final int rowNumber=0x7f070007;
+        public static final int repeatPlusButton=0x7f070006;
+        public static final int rowNumber=0x7f070003;
         public static final int rowPlusButton=0x7f070004;
         public static final int yourOnRepeat=0x7f070001;
-        public static final int yourOnRow=0x7f070006;
+        public static final int yourOnRow=0x7f070002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class string {
-        public static final int Repeat=0x7f040005;
-        public static final int Rows=0x7f040004;
+        public static final int Repeat=0x7f040003;
+        public static final int Rows=0x7f040002;
         public static final int app_name=0x7f040000;
-        public static final int clear=0x7f040006;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int minOne=0x7f040008;
-        public static final int onRepeat=0x7f040009;
-        public static final int onRow=0x7f04000a;
-        public static final int plusOne=0x7f040007;
-        public static final int title_activity_main=0x7f040003;
+        public static final int clear=0x7f040004;
+        public static final int minOne=0x7f040006;
+        public static final int onRepeat=0x7f040007;
+        public static final int onRow=0x7f040008;
+        public static final int plusOne=0x7f040005;
+        public static final int title_activity_main=0x7f040001;
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
